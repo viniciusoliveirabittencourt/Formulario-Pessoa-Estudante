f1487b50c931d065c14c1ae953293b3a347472e7
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2023-08-06 22:06:45 -0300

    adicionando o estado dos componentes do forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [schooling, setSchooling] = useState('Médio');
+  const [experience, setExperience] = useState('');
 
   return (
     <>
       <form>
         <label>
           Nome:
-          <input type='text' />
+          <input value={name} onChange={({ target }) => setName(target.value)} type='text' />
         </label>
         <label>
           E-mail:
-          <input type='text' />
+          <input value={email} onChange={({ target }) => setEmail(target.value)} type='text' />
         </label>
         <label>
           Escolaridade:
-          <select>
+          <select value={schooling} onChange={({ target }) => setSchooling(target.value)}>
             <option value="Médio">Médio</option>
             <option value="Superior">Superior</option>
             <option value="Pós-graduação">Pós-graduação</option>
@@ -23,7 +28,7 @@ function App() {
         </label>
         <label>
           Resumo das experiências:
-          <textarea />
+          <textarea value={experience} onChange={({ target }) => setExperience(target.value)} />
         </label>
         <button>Enviar!</button>
       </form>
