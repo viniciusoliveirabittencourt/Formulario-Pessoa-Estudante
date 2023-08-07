06a65b89c5e66ae3bc9a18b92a8b0376fe707ec3
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2023-08-06 22:00:31 -0300

    adicionando corpo da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import './App.css'
 
 function App() {
+
   return (
     <>
-      <h1>Inicializando</h1>
+      <form>
+        <label>
+          Nome:
+          <input type='text' />
+        </label>
+        <label>
+          E-mail:
+          <input type='text' />
+        </label>
+        <label>
+          Escolaridade:
+          <select>
+            <option value="Médio">Médio</option>
+            <option value="Superior">Superior</option>
+            <option value="Pós-graduação">Pós-graduação</option>
+          </select>
+        </label>
+        <label>
+          Resumo das experiências:
+          <textarea />
+        </label>
+        <button>Enviar!</button>
+      </form>
     </>
   )
 }
